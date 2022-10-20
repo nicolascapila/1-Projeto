a661a6c9ecf6f4c0f879cc65346b8c0f645e6562
Nicolas Vailant Dutra  Capila <84550737+nicolascapila@users.noreply.github.com>
2022-10-20 14:51:04 -0400

    Programei a opção 1 de saudação

--- Primeiro projeto GITHUB/Program.cs
+++ Primeiro projeto GITHUB/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("-----------------------");
+Console.WriteLine("[1] - saudação");
+Console.WriteLine("-----------------------");
+
+Console.Write("escolha uma opção: ");
+int opc = Convert.ToInt32(Console.ReadLine());
+
+if(opc == 1)
+{
+    Console.WriteLine("qual e o nome?");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("ola" + nome + "!");
+}
